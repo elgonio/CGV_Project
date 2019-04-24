e4a3f5779ce6c53ce4af3a035f7ea43044b9c3a2
Omer Elgoni <omer.elgoni@gmail.com>
2019-04-24 19:23:55 +0200

    Completed the work on classes
    Balls can now move and change colour again.
    movement is also no longer instant
    the window should resize itself too

--- player.js
+++ player.js
@@ -1,12 +1,13 @@
 class PlayerBall {
     
-
     // colour is of the form "red","green" or "blue"
     // position is the position in the detination array
     constructor(colour,position) {
         this.colour = colour;
         this.position = position;
+        this.isMoving = false;
 
+        // what direction is the player moving
         this.controls = {
             moveForward: false,
             moveBackward: false,
@@ -14,19 +15,19 @@ class PlayerBall {
             moveRight: false
         };
     
+        // the positions that a block might occupy
         this.destinations = [-10,-5,0,5,10];
-    
-    
-        this.dest = 3;
+        this.dest = position;
 
-        var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+        var geometry = new THREE.SphereGeometry( 1.5, 32, 32 );
         var material = new THREE.MeshLambertMaterial( { color: 0xaaaaaa, 
                                                         emissive: 0xffffff,
                                                         emissiveIntensity: 1,
                                                         side: THREE.DoubleSide
                                                         } );
         this.sphere = new THREE.Mesh( geometry, material );
-        this.sphere.scale.set(0.5,0.5,0.5);
+
+        this.sphere.position.set(this.destinations[this.dest],0,0);
 
         if(colour == "red")
         {
@@ -44,17 +45,60 @@ class PlayerBall {
 
     handleMovement()
     {
-        if (red_ball.position.x > destinations[dest])
+        // check if the player wants to move and isn't already moving
+        if(this.isMoving == false)
         {
-            red_ball.translateX(-1);
+            if (this.controls.moveLeft == true)
+            {
+                // we only want to move if the player isnt in the leftmost lane
+                if(this.dest > 0)
+                {
+                    this.dest = this.dest - 1;
+                }
+            }
+            else if (this.controls.moveRight == true)
+            {
+                // same idea as bove but for the rightmost lane
+                if(this.dest < 4)
+                {
+                    this.dest = this.dest + 1;
+                }
+            }
         }
-        else if (red_ball.position.x < destinations[dest])
+        
+        
+        // we don't want to just jump to the next position so we move there over time 
+        if (this.sphere.position.x > this.destinations[this.dest] )
         {
-            red_ball.translateX(1);
+            this.sphere.translateX(-0.5);
         }
+        else if (this.sphere.position.x < this.destinations[this.dest])
+        {
+            this.sphere.translateX(+0.5);
+        }
+
+        // THIS SOLVES A BUG. PLZ DONT REMOVE
+        var close_enough = Math.abs(this.sphere.position.x - this.destinations[this.dest]) < 0.1;
+
+        // check if we've reached our destination
+        if(close_enough)
+        {
+            this.isMoving = false;
+            //console.log("stopped");
+        }
+        else
+        {
+            this.isMoving = true;
+            //console.log("in motion");
+        }
+
+        //console.log(this.colour);
+        //console.log(this.dest);
+        //console.log(this.sphere.position.x);
     }
 
-    get_model()
+    // a getter for the mesh
+    get_mesh()
     {
         return this.sphere;
     }
