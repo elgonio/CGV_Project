833edfe0ab7eebce466d4203a8f7fbb1f188481f
Nandi <1064787@students.wits.ac.za>
2019-04-26 12:56:32 +0200

    Started lighting class using index test scene
    Just started, this class ideally covers:
    - Ambient
    - Point
    - Directional lights
    with their respective shadow settings

---
+++ js/LightShadow.js
@@ -0,0 +1,12 @@
+/* class for creating different lighting and settings
+        - light and soft shadows
+        - point light
+        - directional light
+        - ambient lighting
+*/
+
+class Light {
+    constructor() {
+        
+    }
+}
