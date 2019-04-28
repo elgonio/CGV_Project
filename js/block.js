d0f52d5b6fbef934f10f75fffb2924206bd9d26d
Dhevan <1433596@students.wits.ac.za>
2019-04-28 15:38:21 +0200

    Update block.js

--- js/block.js
+++ js/block.js
@@ -1,6 +1,6 @@
 class block{
 
-	// colour is of the form "blue","red","yellow","purple","orange","green"
+	// colour is of the form "blue","red","yellow","purple","orange","green","white" and "grey"
 	// position is the position in the destination array
 	// type dictates the behaviour of the block ("standard", "moving" (side to side movement, "fast"))
 	constructor(colour, position, type){
@@ -16,17 +16,21 @@ class block{
                                                         emissive: 0xffffff,
                                                         emissiveIntensity: 1
                                                         } );
-		this.cube = new THREE.Mesh( geometry, material );
+		this.mesh = new THREE.Mesh( geometry, material );
 
-        this.sphere.position.set(this.destinations[this.dest],0,100);
+        this.mesh.position.set(this.destinations[this.dest],0,50); //Alter intial starting z-position with playtesting
 
+        //Change the block to the colour it was assigned
         switch(colour){
+        	case "grey":
+	            this.sphere.material.emissive.setHex(0xaaaaaa);
+        		break;
         	case "blue":
 	            this.sphere.material.emissive.setHex(0x0000ff);
         		break;
 
         	case "red":
-        		this.sphere.material.emissive.setHex(0x0000ff);
+        		this.sphere.material.emissive.setHex(0xff0000);
         		break;
 
         	case "yellow":
@@ -45,6 +49,32 @@ class block{
         		this.sphere.material.emissive.setHex(0x00ff00);
         		break;
 
+    		case "white":
+    			this.sphere.material.emissive.setHex(0xffffff);
+        		break;
+
         }
 	}
+
+	handleMovement(){
+		if(this.type == "standard"){
+			//Move down the track
+	        if (this.mesh.position.z > 0 )
+	        {
+	            this.mesh.translateZ(-0.5); // NEED TO CHANGE THIS TO MATCH SPEED WITH AND PLAY TEST THE HELL OUT OF THIS
+	        }
+   	 	}
+   	 	else if(this.type == "moving"){
+   	 		//move in a sin wave from x = -5 and x = 5
+   	 	}
+   	 	else if(this.type == "fast"){
+   	 		//a single block charges straight down the track (overtakes previously instantiated rows)
+   	 	}
+	}
+
+    // a getter for the mesh
+    get_mesh()
+    {
+        return this.mesh;
+    }
 }
