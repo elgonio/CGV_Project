227b211e8467b56731bc5f9ed9e07b7719f26774
Dhevan <1433596@students.wits.ac.za>
2019-04-28 18:03:15 +0200

    Create tracklines.js

---
+++ js/tracklines.js
@@ -0,0 +1,44 @@
+//Object to group the individial tracks together
+var trackObjects = THREE.Object3D();
+
+class Tracks{
+	constructor{
+
+		var trackGeometry, trackMaterial;
+		this.obsTrackMeshArray = []
+		this.playerTrackMeshArray = [];
+
+		var trackGeometry = new THREE.BoxGeometry( 0.2, 0.2, 100);
+		var trackMaterial = new THREE.MeshLambertMaterial( { color: 0x444444, 
+                                                        emissive: 0xffffff,
+                                                        emissiveIntensity: 1,
+                                                        side: THREE.DoubleSide
+                                                        } );
+		
+		//Create track lines indicate the paths of incoming block obstacles
+		var xPos = -10;
+		for (var i = 0; i <= 4; i++) {
+			this.obsTrackMeshArray[i] = new THREE.Mesh(trackGeometry, trackMaterial);
+			this.obsTrackMeshArray[i].position.set(xPos, -2, 50);
+			trackObjects.add(this.obsTrackMeshArray[i]);
+			xPos += 5;
+		}
+
+		//Change from vertical to horizontal track geometry
+		trackGeometry = new THREE.BoxGeometry( 25, 0.2, 0.2);
+
+		//Create track lines indicate the paths of that the player can move on horizontally
+		var zPos = 5;
+		for (var i = 0; i <= 1; i++) {
+			this.playerTrackMeshArray[i] = new THREE.Mesh(trackGeometry, trackMaterial);
+			this.playerTrackMeshArray[i].position.set(0, -2, zPos);
+			trackObjects.add(this.playerTrackMeshArray[i]);
+			zPos -= 5;
+		}
+
+	}
+
+	get_Tracks(){
+		return trackObjects;
+	}
+}
