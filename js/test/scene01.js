248be8492b06602b175daae6d879d63e4ff869ed
Nandi <1064787@students.wits.ac.za>
2019-05-13 11:44:20 +0200

    Particles wip

--- js/test/scene01.js
+++ js/test/scene01.js
@@ -1,9 +1,10 @@
 // class to initialise the test scene
 class SceneInit {
-    constructor(scene, camera, renderer) {
+    constructor(scene, camera, renderer, clock) {
         this.scene = scene;
         this.camera = camera;
         this.renderer = renderer;
+        this.clock = clock;
         //this.controls = controls;
     }
 
@@ -24,6 +25,9 @@ class SceneInit {
         // add support for shadow mapping in renderer
         this.renderer.shadowMap.enabled = true;
         this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+        // timer and clock enabling
+        this.clock = new THREE.Clock();
 
         // append renderer as dominant element
         document.body.appendChild( this.renderer.domElement );
@@ -87,7 +91,8 @@ class SceneInit {
 
     // particle system demo
     addParticles() {
-        let clock = new THREE.Clock();
+        //let clock = new THREE.Clock();
+        let tick = 0.0;
         let options, spawnerOptions, particleSystem;
 
         particleSystem = new THREE.GPUParticleSystem( {
@@ -153,6 +158,25 @@ class SceneInit {
 
     animate() {
         requestAnimationFrame( this.animate.bind(this) );
+
+        /*
+        var delta = this.clock.getDelta() * this.spawnerOptions.timeScale;
+        tick += delta;
+
+        if ( tick < 0 ) tick = 0;
+
+        if ( delta > 0 ) {
+            this.options.position.x = Math.sin( tick * this.spawnerOptions.horizontalSpeed ) * 10;
+            this.options.position.z = Math.sin( tick * this.spawnerOptions.verticalSpeed ) * 10;
+            this.options.position.y = Math.sin( tick * this.spawnerOptions.horizontalSpeed + this.spawnerOptions.verticalSpeed ) * 5;
+            
+            for ( var x = 0; x < this.spawnerOptions.spawnRate * delta; x++ ) {
+                this.particleSystem.spawnParticle( options );
+            }
+        }
+
+        this.particleSystem.update( tick ); */
+
         this.render();
         //this.controls.update();
     }
